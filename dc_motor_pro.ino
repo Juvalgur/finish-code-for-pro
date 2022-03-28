28e6b5aff18b874011a9b7b591b354a9e7d61d0c
Juvalgur <102517409+Juvalgur@users.noreply.github.com>
2022-03-28 13:02:55 +0300

    Add files via upload
    first

---
+++ dc_motor_pro.ino
@@ -0,0 +1,16 @@
+#define MOTOR enA  9
+#define MOTOR in1  7
+#define MOTOR in2  6
+
+
+void setup() {
+  pinMode(MOTOR enA, OUTPUT);
+  pinMode(MOTOR in1, OUTPUT);
+  pinMode(MOTOR in2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+}
